b86be0a242c2e5d415c9b67f443b4533488e02ce
teo <maia.rosca@gmail.com>
2026-07-20 18:30:07 +0300

    add constraints; change user to the django prebuilt one, change many to many relationship from default implementation to actual table as to have board roles

--- boards/models.py
+++ boards/models.py
@@ -1,31 +1,36 @@
 from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    phone = models.IntegerField(unique=True)
-    joined_date = models.DateField()
+from django.conf import settings
+from django.core.exceptions import ValidationError
 
 class Board(models.Model):
     name = models.CharField(max_length=100)
-    created_date = models.DateField()
+    created_date = models.DateTimeField()
+
+class User_Board(models.Model):
+    role = models.CharField(max_length=30)
     
-    # a user can have more projects (each project has a board so they become unanimous) and each project can have more users
-    users = models.ManyToManyField(User, verbose_name="list of users working on the project") 
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    board = models.ForeignKey(Board)
+    
 
 class Column(models.Model): # things like: to do, done, bugs etc.
     name = models.CharField(max_length=50)
     key = models.CharField(max_length=5)
     position = models.IntegerField()
 
-    board = models.ForeignKey(Board, on_delete=models.CASCADE, unique=True) # should not be able to delete boards if it has columns 
+    board = models.ForeignKey(Board, on_delete=models.CASCADE) # deleting boards deletes evertything
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["board", "key"], name="unique_column_key_for_board")
+        ]
 
 class Task(models.Model):
+    title = models.CharField(max_length=30)
     description = models.CharField(max_length=600)
-    created_date = models.DateField()
-    deadline = models.DateField()
+    created_date = models.DateTimeField()
+    last_edited_date = models.DateTimeField()
+    deadline = models.DateTimeField()
     number = models.IntegerField()
     position = models.FloatField()
 
@@ -33,6 +38,12 @@ class Task(models.Model):
     status = models.ForeignKey(Column, on_delete=models.PROTECT) # error when deleting column with tasks inside 
     main_task = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True, related_name="subtasks") # for subtask integration = adjacency list!!!
 
-    def validate_main_task(self): # keep only one level which is task-> subtask (no task->subtask->subtask->......)
+    def clean(self): # keep only one level which is task-> subtask (no task->subtask->subtask->......)
         if self.main_task and self.main_task.main_task:
-            raise ValueError("Cannot give subtasks to another subtask")
+            raise ValidationError("Cannot give subtasks to another subtask")
+        
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["board", "number"], name="unique_task_number_for_board")
+        ]
+
