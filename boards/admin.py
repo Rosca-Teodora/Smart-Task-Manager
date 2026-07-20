09f3a66d9e2e2729a79af1e87108d930fa1668bd
teo <maia.rosca@gmail.com>
2026-07-20 20:17:47 +0300

    register in admin.py and add data to local SQLite db

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from . models import *
 
-# Register your models here.
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ["key", "name", "created_date"] # what appears in the admin list view
+
+
+@admin.register(UserBoard)
+class UserBoardAdmin(admin.ModelAdmin):
+    list_display = ["user", "board", "role"]
+
+
+@admin.register(Column)
+class ColumnAdmin(admin.ModelAdmin):
+    list_display = ["name", "board", "position"]
+    list_filter = ["board"]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "board", "status", "position", "deadline"]
+    list_filter = ["board", "status"]
+
+
+@admin.register(AssignedTask)
+class AssignedTaskAdmin(admin.ModelAdmin):
+    list_display = ["task", "user"]
